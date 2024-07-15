70a526d2130146b09a79cb76b834fa64886ed40d
Michael Jiang <michaeljiang949@gmail.com>
2024-07-14 23:34:40 -0400

    add README

---
+++ README.md
@@ -0,0 +1,7 @@
+# LeetCode Practice
+
+Here's a running list of the LeetCode questions I've worked on! Feel free to peruse and drop me a line if you've ever like to chat with me about any of my solutions, whether they're clever or (too often) messy :)
+
+Number of questions solved so far: 153
+
+This repo was created using [this](https://github.com/QasimWani/LeetHub) chrome extension.
---
+++ update_README.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Count the number of folders
+count=$(find . -maxdepth 1 -type d | wc -l)
+# Subtract 1 to exclude the current directory (.)
+count=$((count - 1))
+
+# Path to the README file
+readme_file="README.md"
+
+# Update the README.md file with the current folder count
+sed -i '' "s/Number of questions solved so far:.*/Number of questions solved so far: $count/" $readme_file
