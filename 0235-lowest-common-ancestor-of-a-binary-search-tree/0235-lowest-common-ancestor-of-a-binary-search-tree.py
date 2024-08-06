c6eafc0ddb34b68f7a8da899a760dfe04d8fae74
Michael Jiang <55106125+c23mj@users.noreply.github.com>
2024-08-06 15:31:01 -0400

    Time: 50 ms (82.72%), Space: 20.4 MB (24.56%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,25 +7,19 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        queue = deque()
-        queue.append(root);
-        i = 0
-        # lowest = root
-        while(queue):
-            for i in range(len(queue)):
-                curr = queue.popleft()
-                if not curr:
-                    continue
-                print(curr.val)
-                if curr.val == p.val or curr.val == q.val:
-                    return curr
-    
-                if p.val > curr.val and q.val > curr.val:
-                    queue.append(curr.right)
+        frontier = deque([root])
+        while frontier:
+            curr = frontier.popleft() 
+            if curr.val == p.val or curr.val == q.val or \
+               q.val < curr.val < p.val or p.val < curr.val < q.val:
+                return curr
+            if p.val < curr.val and q.val < curr.val:
+                frontier.append(curr.left)
+            else:
+                frontier.append(curr.right)
+        return None
 
-                elif p.val < curr.val and q.val < curr.val:
-                    queue.append(curr.left)
-
-                else:
-                    return curr
-                
+        
+        
+        
+        
