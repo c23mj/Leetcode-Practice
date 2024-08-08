bf326cdd4d01bff9ce24b46c1643078998af3b3b
Michael Jiang <55106125+c23mj@users.noreply.github.com>
2024-08-08 16:54:39 -0400

    Time: 46 ms (85.35%), Space: 17 MB (38.75%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,28 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        cols, posDiag, negDiag = set(), set(), set()
+        res = []
+        board = [['.' for i in range(n)] for j in range(n)]
+
+        def backtrack(r):
+            if r == n:
+                res.append([''.join(row) for row in board])
+            for c in range(n):
+                if c not in cols and r + c not in posDiag and r - c not in negDiag:
+                    board[r][c] = 'Q'
+                    cols.add(c)
+                    posDiag.add(r + c)
+                    negDiag.add(r - c)
+
+                    backtrack(r + 1)
+
+                    board[r][c] = '.'
+                    cols.remove(c)
+                    posDiag.remove(r + c)
+                    negDiag.remove(r - c)
+        backtrack(0)
+        return res
+
+
+
+
