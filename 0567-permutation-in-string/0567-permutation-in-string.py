21777e75f3bb2350b6067832b8ff04bf4a156c50
Michael Jiang <55106125+c23mj@users.noreply.github.com>
2024-08-11 16:27:52 -0400

    Time: 1121 ms (20.04%), Space: 16.7 MB (54.92%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,23 +1,22 @@
 from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        if len(s1) == len(s2):
+            return Counter(s1) == Counter(s2)
+        
+        # sliding window
         s1_counts = Counter(s1)
-        # print(f"s1_counts: {s1_counts}")
-        window_counts = Counter()
-        n = len(s2)
-        l, r = 0, 0
-        while r < n:
-            # print(f"loop start. window_counts: {window_counts}")
-            while r < n and window_counts & s1_counts == window_counts:
-                window_counts[s2[r]] += 1
-                # print(f"incrementing. window_counts: {window_counts}")
+        s2_counts = Counter()        
+        l = r = 0
+        while r < len(s2):
+            while r < len(s2) and s2_counts & s1_counts == s2_counts:
+                s2_counts[s2[r]] += 1
                 r += 1
-                if window_counts == s1_counts:
+                if s2_counts == s1_counts:
                     return True
-            while l < r and window_counts & s1_counts != window_counts:
-                window_counts[s2[l]] -= 1
-                # print(f"decrementing. window_counts: {window_counts}")
-
+            while l < r and s2_counts & s1_counts != s2_counts:
+                s2_counts[s2[l]] -= 1
                 l += 1
         return False
-            
