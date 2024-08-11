21f30666a48380c05cb495857c57c8cb4a199873
Michael Jiang <55106125+c23mj@users.noreply.github.com>
2024-08-11 15:20:48 -0400

    Time: 32 ms (82.13%), Space: 16.5 MB (37.00%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,4 +1,11 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        # print()
-        return int((str(bin(n))[2:].zfill(32)[::-1]), 2)
+        # string comprehension
+        # return int((str(bin(n))[2:].zfill(32)[::-1]), 2)
+        
+        # bit manipulation
+        res = 0
+        for i in range(32):
+            nextBit = (n >> i) & 1
+            res += (nextBit << (31 - i))
+        return res
