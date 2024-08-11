25159a199fb122d8dcee9c94964d410020282ff3
Michael Jiang <55106125+c23mj@users.noreply.github.com>
2024-08-11 12:39:34 -0400

    Time: 103 ms (71.30%), Space: 17.7 MB (39.49%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,9 +1,11 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         n = len(nums)
-        memo = [float('inf') for _ in range(n)]
-        memo[-1] = 0
-        for i in range(n - 2, -1, -1):
-            memo[i] = 1 + min(memo[i: min(n, i + nums[i] + 1)])
-        print(memo)
-        return memo[0]
+        jumps = currentMax = nextMax = 0
+        for i in range(n - 1):
+            nextMax = max(nextMax, i + nums[i])
+            if i == currentMax:
+                jumps += 1
+                currentMax = nextMax
+        return jumps
+    
