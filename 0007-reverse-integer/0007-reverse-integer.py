0584dc7fc6f450ab8664f08d3e93dc3edbd01f51
Michael Jiang <55106125+c23mj@users.noreply.github.com>
2024-08-11 12:53:53 -0400

    Time: 29 ms (91.81%), Space: 16.5 MB (38.04%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -2,16 +2,21 @@ class Solution:
     def reverse(self, x: int) -> int:
         MAX_INT = 2 ** 31 - 1
         MIN_INT = -2 ** 31
-        # print(MAX_INT)
-        # print(MIN_INT)
-        res = 0
-        while x:
-            dig = int(math.fmod(x, 10))
-            x = int(x / 10)
-            if res > MAX_INT // 10 or (res == MAX_INT // 10 and dig > MAX_INT % 10) or \
-               res < MIN_INT // 10 or (res == MIN_INT // 10 and dig < MIN_INT % 10):
-                return 0
-            res = res * 10 + dig
-        return res
-        
+#     Repeated division sol
+#         res = 0
+#         while x:
+#             dig = int(math.fmod(x, 10))
+#             x = int(x / 10)
+#             if res > MAX_INT // 10 or (res == MAX_INT // 10 and dig > MAX_INT % 10) or \
+#                res < MIN_INT // 10 or (res == MIN_INT // 10 and dig < MIN_INT % 10):
+#                 return 0
+#             res = res * 10 + dig
+#         return res
+
+#     String manipulation sol
+        rev_string = str(x)[::-1]
+        if x < 0:
+            rev_string = '-' + rev_string[:-1]
+        res = int(rev_string)
+        return 0 if res > MAX_INT or res < MIN_INT else res
                 
