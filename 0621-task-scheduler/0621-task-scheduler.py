def2d6b3dc2f03cfa12ae98e587406828b8396cb
Michael Jiang <55106125+c23mj@users.noreply.github.com>
2024-08-15 18:11:03 -0400

    Time: 483 ms (32.62%), Space: 16.9 MB (95.44%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,18 +1,44 @@
-from collections import defaultdict, Counter
+from collections import Counter, deque
+import heapq
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        lastCompleted = defaultdict(lambda: float('-inf'))
-        taskCounter = Counter(tasks)
+        # Brute Force Simulation
+        # lastCompleted = defaultdict(lambda: float('-inf'))
+        # taskCounter = Counter(tasks)
+        # time = 0
+        # while taskCounter:
+        #     time += 1
+        #     canDo = [key for key in taskCounter.keys() if lastCompleted[key] < time - n]
+        #     if not canDo:
+        #         continue
+        #     nextTask = max(canDo, key = lambda x: taskCounter[x])
+        #     taskCounter[nextTask] -= 1
+        #     if taskCounter[nextTask] == 0:
+        #         del taskCounter[nextTask]
+        #     lastCompleted[nextTask] = time
+        # return time
+        
+        # With a heap and a queue 
+        counts = [-val for val in Counter(tasks).values()]
+        heapq.heapify(counts) # max heap of cooldowns
+        cooldownQueue = deque()
         time = 0
-        while taskCounter:
+        while counts or cooldownQueue:
+            if counts:
+                nextCount = heapq.heappop(counts)
+                if nextCount < -1:
+                    cooldownQueue.append((nextCount + 1, time + n + 1)) 
             time += 1
-            canDo = [key for key in taskCounter.keys() if lastCompleted[key] < time - n]
-            if not canDo:
-                continue
-            nextTask = max(canDo, key = lambda x: taskCounter[x])
-            taskCounter[nextTask] -= 1
-            if taskCounter[nextTask] == 0:
-                del taskCounter[nextTask]
-            lastCompleted[nextTask] = time
+            while cooldownQueue and cooldownQueue[0][1] <= time:
+                heapq.heappush(counts, cooldownQueue.popleft()[0])
         return time
+            
+            
+            
+        
+        
+        
+        
+        
+        
         
