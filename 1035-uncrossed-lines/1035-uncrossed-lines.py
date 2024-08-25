a55f5af550a56adc42871fb5f9014f050ad9ebed
Michael Jiang <55106125+c23mj@users.noreply.github.com>
2024-08-25 11:44:33 -0400

    Time: 100 ms (97.00%), Space: 16.5 MB (95.67%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.py
+++ 1035-uncrossed-lines/1035-uncrossed-lines.py
@@ -1,14 +1,42 @@
 class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        if len(nums2) > len(nums1):
+            nums1, nums2 = nums2, nums1
+        # space-optimized 2n dp
         m, n = len(nums1), len(nums2)
-        memo = {}
-        def countAtIndices(i: int, j: int):
-            if i >= m or j >= n:
-                return 0
-            if (i, j) not in memo:
-                if nums1[i] == nums2[j]:
-                    memo[(i, j)] = 1 + countAtIndices(i+1, j+1)
+        prev = [0] * (n + 1)
+        for i in range(1, m + 1):
+            curr = [0] * (n + 1)
+            for j in range(1, n + 1):
+                if nums1[i - 1] == nums2[j - 1]:
+                    curr[j] = 1 + prev[j - 1]
                 else:
-                    memo[(i, j)] = max(countAtIndices(i, j + 1), countAtIndices(i + 1, j))
-            return memo[(i, j)]
-        return countAtIndices(0, 0)
+                    curr[j] = max(prev[j], curr[j-1])
+            prev = curr
+        return curr[-1]      
+                    
+
+        # iterative mn dp
+        # m, n = len(nums1), len(nums2)
+        # memo = [[0] * (n + 1) for _ in range(m + 1)]
+        # for i in range(1, m + 1):
+        #     for j in range(1, n + 1):
+        #         if nums1[i - 1] == nums2[j - 1]:
+        #             memo[i][j] = 1 + memo[i-1][j-1]
+        #         else:
+        #             memo[i][j] = max(memo[i-1][j], memo[i][j-1])
+        # return memo[-1][-1]        
+                    
+        
+        # m, n = len(nums1), len(nums2)
+        # memo = {}
+        # def countAtIndices(i: int, j: int):
+        #     if i >= m or j >= n:
+        #         return 0
+        #     if (i, j) not in memo:
+        #         if nums1[i] == nums2[j]:
+        #             memo[(i, j)] = 1 + countAtIndices(i+1, j+1)
+        #         else:
+        #             memo[(i, j)] = max(countAtIndices(i, j + 1), countAtIndices(i + 1, j))
+        #     return memo[(i, j)]
+        # return countAtIndices(0, 0)
