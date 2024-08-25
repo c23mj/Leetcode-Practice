cb9dd191be70d297f1ed578f4127a8b427c590c1
Michael Jiang <55106125+c23mj@users.noreply.github.com>
2024-08-24 23:14:01 -0400

    Time: 461 ms (47.01%), Space: 18 MB (71.52%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -1,38 +1,29 @@
-from collections import deque
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
         deads = set(deadends)
-        seen = set(['0000'])
-        # print(seen)
-        if '0000' in deads: return -1
-        def getAdjs(combo: str):
-            adjs = []
+        if '0000' in deads:
+            return -1
+        seen = {'0000'}
+        def adjs(combo: str) -> List[str]:
+            validAdjs = []
             for i in range(4):
-                upOne = combo[0:i] + (str((int(combo[i]) + 1) % 10)) \
-                        + combo[i+1:]
-                downOne = combo[0:i] + (str((int(combo[i]) + 9) % 10)) \
-                        + combo[i+1:]
-                if upOne not in deads and upOne not in seen: 
-                    seen.add(upOne)
-                    adjs.append(upOne)
-                if downOne not in deads and downOne not in seen:
-                    seen.add(downOne)
-                    adjs.append(downOne)
-            return adjs
-
+                currDigit = int(combo[i])
+                upOne = combo[:i] + str((currDigit + 1) % 10) + combo[i+1:]
+                downOne = combo[:i] + str((currDigit - 1) % 10) + combo[i+1:]
+                for adj in [upOne, downOne]:
+                    if adj not in seen and adj not in deads:
+                        seen.add(adj)
+                        validAdjs.append(adj)
+            return validAdjs
         frontier = deque([('0000', 0)])
         while frontier:
-            currCombo, currDepth = frontier[0]
-            frontier.popleft()
-            # print(f"currCombo: {currCombo}, currDepth: {currDepth}")
-            if currCombo == target: 
-                return currDepth
-            adjacents = [(adjCombo, currDepth + 1) for adjCombo in getAdjs(currCombo)]
-            frontier.extend(adjacents)
-            
+            combo, cost = frontier.popleft()
+            if combo == target:
+                return cost
+            frontier.extend([(c, cost + 1) for c in adjs(combo)])
         return -1
         
+            
+        
                 
-
-        
-        
+                
